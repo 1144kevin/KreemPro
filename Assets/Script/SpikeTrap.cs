7b293659674e8e887d153ebb89c008b80398f948
1144kevin <s111019009@stu.ntue.edu.tw>
2024-11-22 22:51:20 +0800

    11/22

--- Assets/Script/SpikeTrap.cs
+++ Assets/Script/SpikeTrap.cs
@@ -8,8 +8,7 @@ public class SpikeTrap : MonoBehaviour
 {
     [SerializeField] private int damage = 10; // 将damage改为int并赋值默认值
     public List<PlayerController> ListCharacters = new List<PlayerController>();
-    // public List<Spike> ListSpikes = new List<Spike>();
-    [SerializeField] private int damage = 10;
+
     [SerializeField] private Animator animator;
     [SerializeField] private float pushForce = 5f; // 玩家被推的力量
 
@@ -21,16 +20,12 @@ public class SpikeTrap : MonoBehaviour
     // public ParticleSystem HittedEffect;
     private void Start()
     {
-        SpikeTriggerRoutine = null;
-        SpikesReload = true;
-        ListCharacters.Clear();
-        // ListSpikes.Clear();
+        isPushed = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController control = other.GetComponent<PlayerController>();
 
-        // Spike[] arr = this.gameObject.GetComponentsInChildren<Spike>();
-        // foreach (Spike s in arr)
-        // {
-        //     ListCharacters.Add(control);
-        // }
         if (isPushed == false)
         {
             if (control != null && control.CompareTag("Player"))
@@ -44,30 +39,6 @@ public class SpikeTrap : MonoBehaviour
         }
 
     }
-
-    // void OnCollisionEnter(Collision collision)
-    // {
-    //     // 檢查碰撞對象是否是玩家
-    //     if (collision.gameObject.CompareTag("Player"))
-    //     {
-    //         Debug.Log("Player hit!");
-
-    //         // 獲取玩家的 Rigidbody
-    //         Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
-
-    //         if (playerRigidbody != null)
-    //         {
-    //             // 計算推動方向（從碰撞點推開玩家）
-    //             Vector3 pushDirection = collision.contacts[0].point - transform.position;
-    //             pushDirection = -pushDirection.normalized; // 反向推動方向，使玩家遠離
-
-    //             // 施加推力
-    //             float pushForce = 10f; // 可調整的推力大小
-    //             playerRigidbody.AddForce(pushDirection * pushForce, ForceMode.Impulse);
-    //         }
-    //     }
-    // }
-
     private IEnumerator TriggerTrap(Collider other)
     {
         // yield return new WaitForSeconds(0.5f);
@@ -115,35 +86,3 @@ public class SpikeTrap : MonoBehaviour
 
     }
 }
-// private void OnTriggerEnter(Collider other)
-// {
-
-//     PlayerController control = other.gameObject.transform.root.gameObject.GetComponent<PlayerController>();
-
-//     if (control != null)
-//     {
-//         if (!ListCharacters.Contains(control))
-//         {
-//             ListCharacters.Add(control);
-//         }
-//     }
-//     if (control.CompareTag("Player"))
-//     {
-//         animator.SetBool("TrapTrigger", true);
-
-//     }       
-
-//      yield return new WaitForSeconds(1f);
-
-//      if (other.CompareTag("Player"))
-//     {
-//         Health playerHealth = other.GetComponent<Health>();
-//         if (playerHealth != null) // 检查是否找到Health组件
-//         {
-//             playerHealth.TakeDamage(damage);
-//         }
-//     }
-
-
-
-// }
