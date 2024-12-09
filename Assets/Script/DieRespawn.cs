8e38ae3a5451c1a9b88b991f9f9d2e7c90639c98
1144kevin <s111019009@stu.ntue.edu.tw>
2024-12-10 00:40:16 +0800

    完成角色重生

--- Assets/Script/DieRespawn.cs
+++ Assets/Script/DieRespawn.cs
@@ -5,29 +5,15 @@ public class DieRespawn : MonoBehaviour
     public Vector3 respawnPosition; // 指定重新生成的位置
     public ParticleSystem respawnedObject;
 
-    void Start()
-    {
-
-    }
-    private void Update()
-    {
-        // // 檢測是否按下 K 鍵
-        // if (Input.GetKeyDown(KeyCode.K))
-        // {
-        //     DestroyAndRespawn();
-        // }
-    }
-
-    // 用於銷毀物件並重新生成的方法，可供其他腳本調用
     public void DestroyAndRespawn()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        transform.position = respawnPosition;
+        transform.rotation = Quaternion.identity; // 可以根據需要調整角度
+        if (respawnedObject != null)
         {
-            transform.position = respawnPosition;
-            transform.rotation = Quaternion.identity;
-            respawnedObject.Play();  // 啟動特效
+            respawnedObject.Play(); // 啟動重生特效
         }
     }
-
 }
+
 
