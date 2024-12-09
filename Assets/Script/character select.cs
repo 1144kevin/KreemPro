95a805d3ec514f16bddb9f9b17ba7ef20ac32368
Shou666 <ja66668888@gmail.com>
2024-12-09 17:36:22 +0800

    shou整合

--- Assets/Script/character select.cs
+++ Assets/Script/character select.cs
@@ -1,75 +1,3 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
-
-public class characterselect : MonoBehaviour
-{
-    [SerializeField] private GameObject[] characters;
-    [SerializeField] private GameObject[] characters1;
-    [SerializeField] private GameObject[] characters2;
-    [SerializeField] private GameObject[] characters3;
-
-
-    [SerializeField] private GameObject[] charactersname;
-    [SerializeField] private GameObject[] charactersname1;
-    [SerializeField] private GameObject[] charactersname2;
-    [SerializeField] private GameObject[] charactersname3;
-
-    [Header("Sounds")]
-    [SerializeField]private AudioClip arrowClickSFX;
-    [SerializeField]private AudioClip characterSelectMusic;
-
-     
-   public void changecharacter(int index)
-   {
-      for(int i=0;i< characters.Length;i++)
-       {
-           characters[i].SetActive(false);
-           charactersname[i].SetActive(false);
-       }
-       characters[index].SetActive(true);
-       charactersname[index].SetActive(true);
-   }
-   
-   public void changecharacter1(int index)
-   {
-      for(int i=0;i< characters1.Length;i++)
-       {
-           characters1[i].SetActive(false);
-           charactersname1[i].SetActive(false);
-       }
-       characters1[index].SetActive(true);
-       charactersname1[index].SetActive(true);
-   }
-
-   public void changecharacter2(int index)
-   {
-      for(int i=0;i< characters2.Length;i++)
-       {
-           characters2[i].SetActive(false);
-           charactersname2[i].SetActive(false);
-       }
-       characters2[index].SetActive(true);
-       charactersname2[index].SetActive(true);
-   }
-
-   public void changecharacter3(int index)
-   {
-      for(int i=0;i< characters3.Length;i++)
-       {
-           characters3[i].SetActive(false);
-           charactersname3[i].SetActive(false);
-       }
-       characters3[index].SetActive(true);
-       charactersname3[index].SetActive(true);
-   }
-   
-}
-*/
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -82,15 +10,15 @@ public class characterselect : MonoBehaviour
     [SerializeField] private GameObject[] characters1;
     [SerializeField] private GameObject[] characters2;
     [SerializeField] private GameObject[] characters3;
-
     [SerializeField] private GameObject[] charactersname;
     [SerializeField] private GameObject[] charactersname1;
     [SerializeField] private GameObject[] charactersname2;
     [SerializeField] private GameObject[] charactersname3;
 
-    [Header("Sounds")]
-    [SerializeField] private AudioClip arrowClickSFX;
-    [SerializeField] private AudioClip characterSelectMusic;
+    [SerializeField] private GameObject[] charactersingame;
+    [SerializeField] private GameObject[] charactersingame1;
+    [SerializeField] private GameObject[] charactersingame2;
+    [SerializeField] private GameObject[] charactersingame3;
 
     // 用於記錄角色是否已被選擇
     private static bool[] isCharacterSelected = new bool[4]; // 假設角色列表有4個角色
@@ -113,8 +41,13 @@ public class characterselect : MonoBehaviour
         characters[index].SetActive(true);
         charactersname[index].SetActive(true);
 
+        for (int i = 0; i < charactersingame.Length; i++)
+        {
+            charactersingame[i].SetActive(false);
+        }
+        charactersingame[index].SetActive(true);
+
         selectedCharacterIndex = index;
-        AudioSource.PlayClipAtPoint(arrowClickSFX, transform.position);
     }
 
     // 玩家 2 的選角
@@ -135,7 +68,6 @@ public class characterselect : MonoBehaviour
         charactersname1[index].SetActive(true);
 
         selectedCharacterIndex = index;
-        AudioSource.PlayClipAtPoint(arrowClickSFX, transform.position);
     }
 
     // 玩家 3 的選角
@@ -156,7 +88,6 @@ public class characterselect : MonoBehaviour
         charactersname2[index].SetActive(true);
 
         selectedCharacterIndex = index;
-        AudioSource.PlayClipAtPoint(arrowClickSFX, transform.position);
     }
 
     // 玩家 4 的選角
@@ -177,23 +108,7 @@ public class characterselect : MonoBehaviour
         charactersname3[index].SetActive(true);
 
         selectedCharacterIndex = index;
-        AudioSource.PlayClipAtPoint(arrowClickSFX, transform.position);
     }
 
-    // 確認選擇按鈕
-    public void ConfirmSelection(int player)
-    {
-        if (selectedCharacterIndex == -1)
-        {
-            Debug.Log("No character selected to confirm!");
-            return;
-        }
 
-        // 設定選擇狀態為已選
-        isCharacterSelected[selectedCharacterIndex] = true;
-        Debug.Log("Player " + player + " confirmed character " + selectedCharacterIndex);
-        
-        // 播放選擇確認音效
-        AudioSource.PlayClipAtPoint(characterSelectMusic, transform.position);
-    }
-}
+}   
