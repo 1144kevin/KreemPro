c2c01d30ec52b7d98188cca2c05a6f24f026802b
1144kevin <s111019009@stu.ntue.edu.tw>
2025-03-31 21:28:49 +0800

    feat:animation sync

--- Assets/Script/AnimationHandler.cs
+++ Assets/Script/AnimationHandler.cs
@@ -10,37 +10,42 @@ public class AnimationHandler : MonoBehaviour
 
     public void PlayerAnimation(Vector3 input)
     {
-        var isMoving = input.magnitude > 0.1f;
-
-        if (isMoving)
-        {
-            animator.SetBool("isMoving", true);
-        }
-        else
-        {
-            animator.SetBool("isMoving", false);
-        }
-        // //攻擊
-        // if (attackInput && !isAttacking) // 確保攻擊動畫只觸發一次
-        // {
-        //     isAttacking = true;
-
-        //     if (isMoving)
-        //     {
-        //         animator.SetTrigger("isRunAttack");
-        //     }
-        //     else
-        //     {
-        //         animator.SetTrigger("isAttack");
-        //     }
-        // }
-        // else if (!attackInput) // 當按鍵釋放時重置狀態
-        // {
-        //     isAttacking = false;
-        //     animator.SetTrigger("unAttack");
-        //     animator.SetTrigger("unRunAttack");
-        // }
-
+        animator.SetBool("isMoving", input.magnitude > 0.1f);
     }
+    
+    // public void PlayerAnimation(Vector3 input)
+    // {
+    // var isMoving = input.magnitude > 0.1f;
+
+    // if (isMoving)
+    // {
+    //     animator.SetBool("isMoving", true);
+    // }
+    // else
+    // {
+    //     animator.SetBool("isMoving", false);
+    // }
+    // //攻擊
+    // if (attackInput && !isAttacking) // 確保攻擊動畫只觸發一次
+    // {
+    //     isAttacking = true;
+
+    //     if (isMoving)
+    //     {
+    //         animator.SetTrigger("isRunAttack");
+    //     }
+    //     else
+    //     {
+    //         animator.SetTrigger("isAttack");
+    //     }
+    // }
+    // else if (!attackInput) // 當按鍵釋放時重置狀態
+    // {
+    //     isAttacking = false;
+    //     animator.SetTrigger("unAttack");
+    //     animator.SetTrigger("unRunAttack");
+    // }
+
+    // }
 
 }
