e1ff4cd9aff49ca6061fed27af8069eead2a4312
Zivswager5945 <nick920120@gmail.com>
2025-04-22 22:02:48 +0800

    把何作的抓下來看

--- Assets/Script/AttackHandler.cs
+++ Assets/Script/AttackHandler.cs
@@ -22,7 +22,7 @@ public class AttackHandler : NetworkBehaviour
     {
         //Debug.Log("shot1");
         objectSpawner.SpawnSphere();
-        
+
         StartCoroutine(DespawnAfterDelay(3f));
         // if (Runner.LagCompensation.Raycast(
         //     CharacterTrans.position, CharacterTrans.forward,
@@ -39,6 +39,43 @@ public class AttackHandler : NetworkBehaviour
         //     }
         // }
     }
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void Rpc_RequestAttack([RpcTarget] PlayerRef requestingPlayer)
+    {
+        PerformAttack(requestingPlayer);
+    }
+    private void PerformAttack(PlayerRef shooter)
+{
+    if (Runner.LagCompensation == null)
+    {
+        Debug.LogError("LagCompensation is null! This should only run on Host.");
+        return;
+    }
+
+    Vector3 rayOrigin = CharacterTrans.position + Vector3.up * 1.0f;
+    Debug.DrawRay(rayOrigin, CharacterTrans.forward * attackDistance, Color.red, 1f);
+
+    if (Runner.LagCompensation.Raycast(
+        rayOrigin,
+        CharacterTrans.forward,
+        attackDistance,
+        shooter,
+        out var hit,
+        HitLayer,
+        HitOptions))
+    {
+        Debug.Log("hit: " + hit.GameObject.name);
+        if (hit.GameObject.TryGetComponent<Player>(out var hitPlayer))
+        {
+            hitPlayer.TakeDamage(damage);
+        }
+    }
+    else
+    {
+        Debug.Log("hit fail");
+    }
+}
+
     private IEnumerator DespawnAfterDelay(float delay)
     {
 
